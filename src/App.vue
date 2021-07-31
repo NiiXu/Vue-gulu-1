cb0a4b72d3d05d240aeac7dcc8b8950d6347faaf
xu <chenhengxv@outlook.com>
2021-07-31 14:01:41 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <chen/>
+</template>
+
+<script>
+import Chen from './components/Chen.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Chen
+  }
+}
+</script>
