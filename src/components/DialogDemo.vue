030e1352f018c2158294af94e0b8274ebf626aec
xu <chenhengxv@outlook.com>
2021-08-10 15:16:55 +0800

    openDialog 一句话打开 Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,23 +2,24 @@
 <div>Dialog 示例</div>
 <h1>示例1</h1>
 <Button @click="toggle">toggle</Button>
-<Dialog v-model:visible="x" :closeOnClickOverlay="false" 
-:ok="f1" :cancel="f2">
-<template v-slot:content>
-  <strong>hi</strong>
-  <div>你好</div>
-</template>
-<template v-slot:title>
-  <strong>加粗的标题</strong>
-</template>
-
+<Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
+  <template v-slot:content>
+    <strong>hi</strong>
+    <div>hi2</div>
+  </template>
+  <template v-slot:title>
+    <strong>加粗的标题</strong>
+  </template>
 </Dialog>
+<h1>示例2</h1>
+<Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
-import {ref} from 'vue'
+import {ref,h} from 'vue'
+import {openDialog} from '../lib/openDialog'
 export default {
   components: {
     Dialog,
@@ -34,12 +35,26 @@ export default {
     }
     const f2 = () => {
     }
+     const showDialog = () => {
+      openDialog({
+        title: h('strong', {}, '标题'),
+        content: '你好',
+        ok() {
+          console.log('ok')
+        },
+        cancel() {
+          console.log('cancel')
+        }
+      })
+    }
     return {
       x,
       toggle,
       f1,
-      f2
+      f2,
+      showDialog
     }
   }
+  
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -59,7 +59,7 @@ export default {
       }
     }
     const cancel = () => {
-      context.emit('cancel')
+      props.cancel && props.cancel()
       close()
     }
     return {
