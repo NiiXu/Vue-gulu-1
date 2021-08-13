bf1a7b97d31f225701cf1d197d68f1c7edfcbe00
xu <chenhengxv@outlook.com>
2021-08-13 14:11:30 +0800

    Tabs 组件优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,11 @@
 <template>
 <div class="gulu-tabs">
   <div class="gulu-tabs-nav" ref="container">
-    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
     <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="gulu-tabs-content">
-    <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
-
+    <component :is="current" :key="current.props.title" />
   </div>
 </div>
 </template>
@@ -16,8 +15,8 @@ import Tab from './Tab.vue'
 import {
   computed,
   ref,
-  onMounted,
-  onUpdated
+  watchEffect,
+  onMounted
 } from 'vue'
 export default {
   props: {
@@ -26,28 +25,25 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref < HTMLDivElement[] > ([])
+    const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      console.log(result)
-      const {
-        width
-      } = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect()
-      const {
-        left: left2
-      } = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -55,9 +51,7 @@ export default {
       }
     })
     const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected
-      })[0]
+      return defaults.find(tag => tag.props.title === props.selected)
     })
     const titles = defaults.map((tag) => {
       return tag.props.title
@@ -66,11 +60,11 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
-      current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container
     }
@@ -111,12 +105,6 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
